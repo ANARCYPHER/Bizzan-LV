593eb1d5430cc59e15d61662d2705becd6cd9c54
AllSafeCybercurity <skull32273227@gmail.com>
2021-07-03 07:44:29 -0700

    install js and vue

---
+++ resources/js/vue/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="todoListContainer">
+        <div class="heading">
+            <h2 id="title">Todo list</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+import addItemForm from "./addItemForm"  
+  
+export default {
+
+}
+</script>
