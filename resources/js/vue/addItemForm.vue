c1d823dcf28bc03c51d6cdf1380e7efbcbeae76d
AllSafeCybercurity <skull32273227@gmail.com>
2021-07-04 07:27:55 -0700

    vue frontend

--- resources/js/vue/addItemForm.vue
+++ resources/js/vue/addItemForm.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
+    <div class="addItem">
+        <input type="text" /> 
         Form
     </div>
 </template>
--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -2,14 +2,36 @@
     <div class="todoListContainer">
         <div class="heading">
             <h2 id="title">Todo list</h2>
+            <add-item-form />
         </div>
+        <list-view />
     </div>
 </template>
 
 <script>
 import addItemForm from "./addItemForm"  
-  
-export default {
+import ListView from "./ListView" 
 
+export default {
+    components: {
+       addItemForm,
+       listView
+    }
 }
 </script>
+
+<style scoped>
+.todoListContainer {
+    width: 350px;
+    margin: auto;
+}
+
+.heading {
+    background: #6e6e6e;
+    padding: 10px;
+}
+
+#title {
+    text-align: center;
+}
+</style>
